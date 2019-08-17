599fd911c1e78972bccb8503022c85ccc06f9730
Francisco V <fvskate@gmail.com>
2019-08-17 14:54:07 -0400

    corrigiendo Readme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function FormatName(user) {
+    return `${user.Nombre} ${user.Apellido}`;
+}
+
+const user = {
+    Nombre: 'Francisco',
+    Apellido: 'Ventura',
+
+}
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hola, {FormatName(user)}!</h1>;
+    }
+    return <h1>Hola, Stranger.</h1>;
+}
+
+
+const element = (
+
+    <div>
+        <h1>Prueba</h1>
+        {getGreeting(user)}
+    </div>
+)
+const container = document.getElementById('root');
+
+ReactDOM.render(element, container);
+
